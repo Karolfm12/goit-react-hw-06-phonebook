96b8a22e6a893706c856828e5206df6329991453
Karolfm12 <karolfm12@onet.eu>
2024-05-12 16:49:51 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [contactList, setContactList] = useState([]);
+
+  const handleOnChange = e => {
+    if (e.target.name === 'firstName') {
+      setName(e.target.value);
+    }
+    if (e.target.name === 'phone') {
+      setPhoneNumber(e.target.value);
+    }
+  };
+
+  const handleOnClick = e => {
+    e.preventDefault();
+    setContactList(prev => [...prev, { name, phoneNumber }]);
+    setName('');
+    setPhoneNumber('');
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <form onSubmit={handleOnClick}>
+        <h2>Name:</h2>
+        <input
+          type="text"
+          name="firstName"
+          value={name}
+          onChange={handleOnChange}
+        />
+        <h2>Phone number:</h2>
+        <input
+          type="number"
+          name="phone"
+          value={phoneNumber}
+          onChange={handleOnChange}
+        />
+        <div>
+          <button type="submit">OK</button>
+        </div>
+      </form>
+      <div>
+        <ul>
+          {contactList.map((value, index) => {
+            return (
+              <li key={index}>
+                {value.name} {value.phoneNumber}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
